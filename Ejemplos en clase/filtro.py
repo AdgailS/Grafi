6f30267e94dd4c0f81db7e02d3ade058a3e5a271
Estrella Sánchez <estrella22abigail@gmail.com>
2026-03-07 14:13:13 -0600

    Avances

---
+++ Ejemplos en clase/filtro.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2 as cv
+
+rostro = cv.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+cap = cv.VideoCapture(0)
+x=y=w=h= 0 
+count = 0
+while True:
+    ret, img = cap.read()
+    gris = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    rostros = rostro.detectMultiScale(gris, 1.3, 5)
+    for(x,y,w,h) in rostros:
+        res = int((w+h)/8)
+        img = cv.rectangle(img, (x,y), (x+w, y+h), (234, 23,23), 5)
+        #img = cv.rectangle(img, (x,int(y+h/2)), (x+w, y+h), (0,255,0),5 )
+        img = cv.circle(img, (x + int(w*0.3), y + int(h*0.4)) , 21, (0, 0, 0), 2 )
+        img = cv.circle(img, (x + int(w*0.7), y + int(h*0.4)) , 21, (0, 0, 0), 2 )
+        img = cv.circle(img, (x + int(w*0.3), y + int(h*0.4)) , 20, (255, 255, 255), -1 )
+        img = cv.circle(img, (x + int(w*0.7), y + int(h*0.4)) , 20, (255, 255, 255), -1 )
+        img = cv.circle(img, (x + int(w*0.3), y + int(h*0.4)) , 5, (0, 0, 255), -1 )
+        img = cv.circle(img, (x + int(w*0.7), y + int(h*0.4)) , 5, (0, 0, 255), -1 )
+       # img = cv.circle(img, (x + int(w*0.5), y + int(h*0.6)), 8, (0, 0, 0), -1) # punto en nariz
+       # CEJAS
+        img = cv.line(img, (x + int(w*0.2), y + int(h*0.20)), (x + int(w*0.4), y + int(h*0.20)), (0,0,0), 3) #izquierda
+        img = cv.line(img, (x + int(w*0.6), y + int(h*0.20)), (x + int(w*0.8), y + int(h*0.20)), (0,0,0), 3) #derecha
+        img = cv.rectangle(img, (x+10,y+10), (x+w, y+h), (234,0 ,234), 5)
+        
+        
+    img2=  img[y:y+h,x:x+w]
+    cv.imshow('img2', img2)
+    cv.imshow('img', img)
+    if cv.waitKey(1)== ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
+
+# DIBUJAR OJOS, BIGOTE, OREJAS Y CEJAS Y NARIZ
+# SEGMENTAR OJOS Y BOCA / ENCERRARLOS EN UN RECTANGULO
